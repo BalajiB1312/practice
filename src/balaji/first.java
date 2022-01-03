131ef91956845db6d7cae221f8c1d5b32aa1421c
Your Name <you@example.com>
2022-01-03 09:06:40 +0530

    first commit

---
+++ src/balaji/first.java
@@ -0,0 +1,15 @@
+package balaji;
+
+public class first {
+
+	
+            int n1=10;
+            int n2=20;
+            public static void main(String[] args) {
+            first sir=new first();
+            
+            System.out.println(sir.n1);
+            System.out.println(sir.n2);
+	}
+
+}
